a701df5d21fd30aeff85b17aac4a4af03745dd24
Jevon Wallace <jw0155039@otc.edu>
2017-11-08 12:43:50 -0600

    newer things

--- index.js
+++ index.js
@@ -13,9 +13,9 @@ $(function () {
             loadName() {
                 this.loading = true
 
-                this.$http.get('http://www.comicvine.com/api/<resource>/?api_key=<775df1eea3760333ce81e39ada323b0e1cd62145>:<value>&format=json')
+                this.$http.get('http://www.comicvine.com/api/<resource>/?api_key=<api_key>&filter=<filter>:<value>&format=json')
                     .then(successCallback, errorCallback)
-
+                    
                     .then(resp => {
                         this.charicters = resp.body
                         this.loading = false
@@ -42,4 +42,5 @@ $(function () {
 
 // 775df1eea3760333ce81e39ada323b0e1cd62145
 // http://www.comicvine.com/api/<resource>/?api_key=<api_key>&filter=<filter>:<value>&format=json
+// https://comicvine.gamespot.com/api/documentation
 // https://comicvine.gamespot.com/api/activate
